da0d7a02407e8e5210c6c1fe9fc193a3d2d5ab6b
Ecmascript Hacker <Ecmascript.Hacker@gmail.com>
2016-02-16 23:06:45 +0545

    HTMLMIN added.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,17 @@ module.exports = function(grunt) {
           "dist/css/index.min.css": "src/css/index.less"
         }
       }
+    },
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'dist/index.html': 'src/index.html',     // 'destination': 'source'
+        }
+      },
     }
   });
 
@@ -29,8 +40,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   // Load the plugin that provides the "less compilation" task.
   grunt.loadNpmTasks('grunt-contrib-less');
+  // Load the plugin that provides the "html minification" task.
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'less']);
+  grunt.registerTask('default', ['uglify', 'less', 'htmlmin']);
 
 };
